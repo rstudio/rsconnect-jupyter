82db9a64730d7ba9a54880ac16079b7735b18b0c
Jonathan Curran <jonathan.curran@rstudio.com>
2018-05-24 11:45:33 -0600

    Disable autocomplete for API Key

--- rsconnect/static/connect.js
+++ rsconnect/static/connect.js
@@ -489,7 +489,7 @@ define([
         "    </div>",
         '    <div class="form-group">',
         "        <label>API Key</label>",
-        '        <input class="form-control" name="api-key" type="text" maxlength="32" required>',
+        '        <input class="form-control" name="api-key" type="text" maxlength="32" autocomplete="off" required>',
         '        <span class="help-block"></span>',
         "    </div>",
         '    <div class="form-group">',
