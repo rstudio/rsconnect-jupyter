fcbae6e378d22b2b35ed7316992101b51548b95c
Michael Marchetti <mike@rstudio.com>
2018-07-27 10:54:14 -0400

    add python and pip versions

--- rsconnect/tests/test_environment.py
+++ rsconnect/tests/test_environment.py
@@ -1,7 +1,12 @@
+import re
+import sys
+
 from unittest import TestCase
 from os.path import dirname, exists, join
 
 from rsconnect.environment import detect_environment
+
+version_re = re.compile(r'\d+\.\d+(\.\d+)?')
 
 class TestEnvironment(TestCase):
     def get_dir(self, name):
@@ -9,12 +14,20 @@ class TestEnvironment(TestCase):
         self.assertTrue(exists(path))
         return path
 
+    def python_version(self):
+    	return '.'.join(map(str, sys.version_info[:3]))
+
     def test_file(self):
         result = detect_environment(self.get_dir('pip1'))
+
+        pip_version = result.pop('pip')
+        self.assertTrue(version_re.match(pip_version))
+
         self.assertEqual(result, {
             'source': 'file',
             'filename': 'requirements.txt', 
             'contents': 'numpy\npandas\nmatplotlib\n',
+            'python': self.python_version(),
         })
 
     def test_pip_freeze(self):
@@ -25,7 +38,11 @@ class TestEnvironment(TestCase):
         self.assertIn('notebook', contents)
         self.assertIn('nbformat', contents)
 
+        pip_version = result.pop('pip')
+        self.assertTrue(version_re.match(pip_version))
+
         self.assertEqual(result, {
             'source': 'pip_freeze',
-            'filename': 'requirements.txt', 
+            'filename': 'requirements.txt',
+            'python': self.python_version(),
         })
