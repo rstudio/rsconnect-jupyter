d2fdb23de6d2674db20c3cfd76b72c4d0a96a45b
Jonathan Curran <jonathan.curran@rstudio.com>
2018-11-07 21:46:12 -0700

    Build HTML docs

---
+++ docs/build-doc.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -ex
+
+mkdir -p dist/html
+cd docs/guide
+
+pandoc -f markdown-implicit_figures \
+    --self-contained \
+    -o ../../dist/rsconnect-jupyter-${VERSION}.html \
+    -H style.css \
+    README.md
+
+pandoc -f markdown-implicit_figures \
+    -o ../../dist/rsconnect-jupyter-${VERSION}.pdf \
+    README.md
