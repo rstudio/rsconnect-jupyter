5e852713a88d84e00588b26ecc09b910f1ee0212
Kevin Gartland <kgartland@rstudio.com>
2018-11-08 16:49:12 -0500

    addressing comments

--- rsconnect/scripts/jupyter_setup.sh
+++ rsconnect/scripts/jupyter_setup.sh
@@ -1,21 +1,22 @@
-#!/bin/sh
-
-if [ `grep $1 versions` ] ; then
-	sudo conda create -y -p /opt/Python/$1 python=$1
-    sudo -H /opt/Python/$1/bin/pip install virtualenv
-else
-	echo Please run with a valid Python Version
-	exit 1
-fi
+#!/bin/bash
 
 # setup miniconda
-sudo curl -o miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-sudo sh miniconda.sh -b -u -p /opt/Python/miniconda
+if ! [ $(which conda) ] ; then
+	echo installing conda
+	curl -o miniconda.sh 'https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh'
+	sh miniconda.sh -u -b
+fi
 
-# setup virtualenv
-PYTHONMAJOR=${1:0:3}
-virtualenv --python=/opt/Python/$1/bin/python$PYTHONMAJOR testenv$1
-source testenv$1/bin/activate
+if [ $(grep $1 versions) ] ; then
+    conda create -n testenv$1 python=$1 anaconda
+    conda install virtualenv
+else
+    echo Please run with a valid Python Version
+    exit 1
+fi
+
+# activate virtualenv
+source activate testenv$1
 
 # get appropriate jupyter plugin
 curl http://s3.amazonaws.com/rstudio-rsconnect-jupyter/>AWS_OUTPUT
@@ -26,33 +27,27 @@ read_dom () {
 }
 
 if [ -z $2 ] ; then
+        CONTENT_ARRAY=()
+        while read_dom ; do
+            if [[ $ENTITY = "Key" ]] ; then
+                for i in $CONTENT ; do
+                        if [[ $CONTENT == *".whl" ]] ; then
+                                 CONTENT_ARRAY+=($CONTENT)
+                        fi
+                    done
+        fi
+        done<AWS_OUTPUT
 
-	CONTENT_ARRAY=()
-	while read_dom ; do
-	    if [[ $ENTITY = "Key" ]] ; then
-	    	for i in $CONTENT ; do
-		        if [[ $CONTENT == *".whl" ]] ; then
-		    		 CONTENT_ARRAY+=($CONTENT)
-		    	fi
-		    done
-    	fi
-	done < AWS_OUTPUT
-
-	echo installing the latest version of the jupyter plugin: ${CONTENT_ARRAY[${#CONTENT_ARRAY[@]}-1]}...
-	sudo -H /opt/Python/$1/bin/pip install https://s3.amazonaws.com/rstudio-rsconnect-jupyter/${CONTENT_ARRAY[${#CONTENT_ARRAY[@]}-1]} || 
-	echo "Latest notebook not available, try again." 
-	exit 1
-
+        echo installing the latest version of the jupyter plugin: ${CONTENT_ARRAY[${#CONTENT_ARRAY[@]}-1]}...
+        pip install https://s3.amazonaws.com/rstudio-rsconnect-jupyter/${CONTENT_ARRAY[${#CONTENT_ARRAY[@]}-1]} || exit 1
 else 
-	echo installing jupyter plugin version $2: https://s3.amazonaws.com/rstudio-rsconnect-jupyter/rsconnect-1.1.0.$2-py2.py3-none-any.whl
-	sudo -H /opt/Python/$1/bin/pip install https://s3.amazonaws.com/rstudio-rsconnect-jupyter/rsconnect-1.1.0.$2-py2.py3-none-any.whl || 
-	echo "$2 may not be a valid notebook version." 
-	exit 1
+        echo installing jupyter plugin version $2: https://s3.amazonaws.com/rstudio-rsconnect-jupyter/rsconnect-1.1.0.$2-py2.py3-none-any.whl
+        pip install https://s3.amazonaws.com/rstudio-rsconnect-jupyter/rsconnect-1.1.0.$2-py2.py3-none-any.whl || exit 1
 fi
 
 # install and run jupyter
-sudo -H /opt/Python/$1/bin/pip install jupyter
-sudo /opt/Python/$1/bin/jupyter-nbextension install --sys-prefix --py rsconnect
-sudo /opt/Python/$1/bin/jupyter-nbextension enable --sys-prefix --py rsconnect
-sudo /opt/Python/$1/bin/jupyter-serverextension enable --sys-prefix --py rsconnect
-/opt/Python/$1/bin/jupyter notebook
+pip install install jupyter
+jupyter-nbextension install --sys-prefix --py rsconnect
+jupyter-nbextension enable --sys-prefix --py rsconnect
+jupyter-serverextension enable --sys-prefix --py rsconnect
+jupyter notebook
