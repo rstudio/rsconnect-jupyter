aca9336ecfb2bbc6cb300e57a7e67e51b82114c8
Michael Marchetti <mike@rstudio.com>
2019-01-25 10:27:10 -0500

    update tests

--- rsconnect_jupyter/tests/test_environment.py
+++ rsconnect_jupyter/tests/test_environment.py
@@ -17,26 +17,8 @@ class TestEnvironment(TestCase):
     def python_version(self):
     	return '.'.join(map(str, sys.version_info[:3]))
 
-    def test_file(self):
-        result = detect_environment(self.get_dir('pip1'))
-
-        pip_version = result.pop('pip')
-        self.assertTrue(version_re.match(pip_version))
-
-        locale = result.pop('locale')
-        self.assertIsInstance(locale, str)
-        self.assertIn('.', locale)
-
-        self.assertEqual(result, {
-            'package_manager': 'pip',
-            'source': 'file',
-            'filename': 'requirements.txt',
-            'contents': 'numpy\npandas\nmatplotlib\n',
-            'python': self.python_version(),
-        })
-
     def test_pip_freeze(self):
-        result = detect_environment(self.get_dir('pip2'))
+        result = detect_environment()
         contents = result.pop('contents')
 
         # these are the dependencies declared in our setup.py
