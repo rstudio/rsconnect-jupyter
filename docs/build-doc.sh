9894831d9f2f62bc04f64bee3542759b6870c3a6
Michael Marchetti <mike@rstudio.com>
2019-02-25 13:34:11 -0500

    simplify docs build script

--- docs/build-doc.sh
+++ docs/build-doc.sh
@@ -2,18 +2,12 @@
 
 set -ex
 
-OUT=../../dist
-
-cd docs/guide
-
-sed s/RSCONNECT_VERSION/${VERSION}/g < README.md > ../../build/docs-README.md
-
 pandoc -f markdown-implicit_figures \
     --self-contained \
-    -o ${OUT}/rsconnect_jupyter-${VERSION}.html \
-    -H style.css \
-    ../../build/docs-README.md
+    -o dist/rsconnect_jupyter-${VERSION}.html \
+    -H docs/images/style.css \
+    README.md
 
 pandoc -f markdown-implicit_figures \
-    -o ${OUT}/rsconnect_jupyter-${VERSION}.pdf \
-    ../../build/docs-README.md
+    -o dist/rsconnect_jupyter-${VERSION}.pdf \
+    README.md
