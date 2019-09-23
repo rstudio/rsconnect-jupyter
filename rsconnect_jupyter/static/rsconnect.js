81acd185f4d748b4911941d79222a9a7877d6210
Michael Marchetti <mike@rstudio.com>
2019-09-23 14:09:08 -0400

    Revert "remove old servers with no API key"
    This reverts commit f331fce6cb9ee89ee7f77ab1e6e8afca7d983370.

--- rsconnect_jupyter/static/rsconnect.js
+++ rsconnect_jupyter/static/rsconnect.js
@@ -173,14 +173,11 @@ define([
                     for (var serverId in data) {
                         // Split out API keys so they're not saved into the notebook metadata.
                         var entry = data[serverId];
+                        self.apiKeys[entry.server] = entry.apiKey;
+                        delete entry.apiKey
 
-                        if (entry.apiKey) {
-                            self.apiKeys[entry.server] = entry.apiKey;
-                            delete entry.apiKey
-
-                            if (!self.servers[serverId]) {
-                                self.servers[serverId] = entry;
-                            }
+                        if (!self.servers[serverId]) {
+                            self.servers[serverId] = entry;
                         }
                     }
                 });
