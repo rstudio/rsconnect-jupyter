3eae8cbf792524c69be11c983d124a78d787527e
Dan Buch <dan@rstudio.com>
2020-06-22 14:52:23 -0400

    Enable docs building + artifact storage

--- docs/build-doc.sh
+++ docs/build-doc.sh
@@ -6,14 +6,14 @@ TITLE='rsconnect-jupyter User Guide'
 
 pandoc -f markdown-implicit_figures \
     --self-contained \
-    -o dist/rsconnect_jupyter-${VERSION}.html \
+    -o docs/out/rsconnect_jupyter-${VERSION}.html \
     -H docs/images/style.fragment.html \
     -T "${TITLE}" \
     -M "title:${TITLE}" \
     README.md
 
 pandoc -f markdown-implicit_figures \
-    -o dist/rsconnect_jupyter-${VERSION}.pdf \
+    -o docs/out/rsconnect_jupyter-${VERSION}.pdf \
     -T "${TITLE}" \
     -M "title:${TITLE}" \
     README.md
